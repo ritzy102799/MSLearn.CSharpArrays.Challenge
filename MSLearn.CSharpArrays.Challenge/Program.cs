cee6428dc118926734cd417b6002add7f1459aa7
ROVIC ROSALES <ritzy102799@gmail.com>
2024-02-20 16:04:27 +0100

    Add project files.

---
+++ MSLearn.CSharpArrays.Challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] fraudalentIDsFrance = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (var item in fraudalentIDsFrance)
+{
+    //Console.WriteLine(item);
+    if (item.StartsWith("A"))
+    {
+        Console.WriteLine(item);
+    }
+}
+foreach (var item in fraudalentIDsFrance)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine(item);
+    }
+}
+foreach (var item in fraudalentIDsFrance)
+{
+    if (item.StartsWith("C"))
+    {
+        Console.WriteLine(item);
+    }
+}
+foreach (var item in fraudalentIDsFrance)
+{
+    if (item.StartsWith("G"))
+    {
+        Console.WriteLine (item);
+    }
+}
+
+
+
+
